80c8aeaa01af6eb57f7c3433a21b0d0f5accd693
Monish-Y <monishy24@gmail.com>
2026-04-23 17:04:03 +0530

    Added Docker

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Jaiharam1.@localhost:5432/fleet_db"
+# DATABASE_URL = "postgresql://postgres:Jaiharam1.@localhost:5432/fleet_db"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/fleet_db"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
